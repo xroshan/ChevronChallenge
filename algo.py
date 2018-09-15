47cc5544d0769223240249d3ae8b0dc7bfe1bc25
xroshan <roshanshrestha937@gmail.com>
2018-09-15 18:22:20 -0500

    phase 1 algorithm ready.. requires extensive testing..

--- algo.py
+++ algo.py
@@ -13,14 +13,22 @@ def get_all_orders():
     orders = priortize_orders(orders)
     return orders
 
+# get all the work orders except the "completed ones" or "in_progress" ones
+def get_all_assigned_orders():
+    orders = Order.query.filter(
+        Order.status != "completed" or Order.status != "in_progress").all()
+    return orders
 
-#sort the work orders based on their priority level
+
+# sort the work orders based on their priority level
 def priortize_orders(orders):
-    return orders.sort(key = priortize_orders_helper)
+    return orders.sort(key=priortize_orders_helper)
 
-#helper to priortize_order function 
+
+# helper to priortize_order function
 def priortize_orders_helper(ord):
     return ord.priority
+
 
 # get all the workers who can fix the "equipment_id"
 def get_all_workers(equipment_id):
@@ -47,7 +55,7 @@ def add_time(a, b, shift_time):
         return add_time(a, b, shift_time)
 
 
-# get the oldest datetime comparing the current old time and datetime if that worker is assigned that work order
+# get estimated start time and estimated end time for a work for that worker
 def get_total_time(TTC, worker_id, res):
     worker = Worker.query.get(worker_id)
     # get the ending shift datetime
@@ -63,13 +71,13 @@ def get_total_time(TTC, worker_id, res):
     return testing_time
 
 
-
 # recover the best solution given the best leaf of a tree
 def recover_best_path(best_node):
     res = []
-    # go back to the root parent tracing from the best leaf and store work_id and worker_id in the list
+    # go back to the root parent tracing from the best leaf and store work_id as key and return worker_id, start_time and end_time in it
     while best_node.depth != 0:
-        res.append([best_node.work_id, best_node.worker_id])
+        res.append([best_node.work_id, [best_node.worker_id,
+                                        best_node.st_time, best_node.end_time]])
         best_node = best_node.parent
     return res
 
@@ -77,7 +85,7 @@ def recover_best_path(best_node):
 # brute force all the scenarios with the help of tree. The path of the best value of the leaf through the tree gives the best scenario
 def get_best_scenario(orders):
     # scenario starts with current datetime
-    scenario = AnyNode(testing_infos = [], value=datetime.now())
+    scenario = AnyNode(testing_infos=[], value=datetime.now())
     depth = 0
 
     for order in orders:
@@ -89,15 +97,16 @@ def get_best_scenario(orders):
             # iterate through all the leaves and grow more based on work assigned
             for partree in partrees:
                 children = []
-                #store all worker_time completion testing as key and value 
+                # store all worker_time completion testing as key and value
                 res = partree.parent.testing_infos
 
                 # if "in_progress", get the latest time comparing the current latest time and time required by worker to complete
                 if order.status == "in_progress":
                     worker = Worker.query.get(order.worker_id)
-                    #set worker testing time to time until free as it has already been assigned "in_progress"
+                    # set worker testing time to time until free as it has already been assigned "in_progress"
                     res[worker.worker_id] = worker.time_until_free
-                    a = AnyNode(testing_infos = res,work_id=order.work_id, worker_id=order.worker_id, value=min(
+                    # node stores all the required variables such as previous work_info, start_time, end_time, worker info and values
+                    a = AnyNode(testing_infos=res, work_id=order.work_id, st_time=datetime.now(), end_time=res[worker.worker_id], worker_id=order.worker_id, value=min(
                         worker.time_until_free, partree.value))
                     children.append(a)
 
@@ -105,10 +114,15 @@ def get_best_scenario(orders):
                 else:
                     workers = get_all_workers(order.equipment_id)
                     for worker in workers:
+                        # if worker has not been assigned earlier work, set his/her leisure time
                         if not worker.worker_id in res:
                             res[worker.worker_id] = worker.time_until_free
-                        res[worker.worker_id] = get_total_time(order.time_to_completion, worker.worker_id, res)
-                        a = AnyNode(testing_infos = res, work_id=order.work_id, worker_id=worker.worker_id, value=max(res[worker.worker_id], partree.value))
+                        # have value for the start_time of work as well as the end time of work
+                        start_time = res[worker.worker_id]
+                        res[worker.worker_id] = get_total_time(
+                            order.time_to_completion, worker.worker_id, res)
+                        a = AnyNode(testing_infos=res, work_id=order.work_id, worker_id=worker.worker_id, st_time=start_time,
+                                    end_time=res[worker.worker_id], value=max(res[worker.worker_id], partree.value))
                         children.append(a)
 
                 # add the leaves to the current parent node
@@ -129,24 +143,38 @@ def get_best_scenario(orders):
     return result
 
 
-#assign the task once we get the best scenario
+# assign the task once we get the best scenario
 def assign_task():
-    orders = get_all_orders()
+    orders = get_all_assigned_orders()
     best = get_best_scenario(orders)
-    #update database
+    # for each order, assign worker, estimated start time and estimated end time
+    for order in orders:
+        order.worker_id = best[order.work_id][0]
+        order.est_start_time = best[order.work_id][1]
+        order.est_end_time = best[order.work_id][2]
+    # update database
+    db.session.commit()
 
-   # during assigning time until free time = time after completion
 
-#check everytime if the task has been completed
-def check_completion():
+# check everytime if the task has been completed or is going to start
+def check_time():
     orders = get_all_orders()
     for order in orders:
-        #if estimated time is greater than current time, then the work is completed
-        if order.est_time > datetime.now():
+        # if estimated end time is greater than current time, then the work is completed
+        if order.est_end_time > datetime.now():
             order.status = 'completed'
-    
 
-#this function gets called when clients adds their work order request
-def main:
-    check_completion()
+        # if estimated start time is greater than current time, then start the work i.e. "in_progress".. worker time_until_free is updated
+        if order.est_start_time > datetime.now():
+            order.status = 'in_progress'
+            worker = Worker.query.get(order.worker_id)
+            worker.time_until_free = order.est_end_time
+    # update database
+    db.session.commit()
+
+
+# this function gets called when clients adds their work order request
+def main():
+    check_time()
     assign_task()
+    check_time()
