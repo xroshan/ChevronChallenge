0bd5810cd04bd9e7690aebd4f832cb70faf9b29d
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-09-15 14:27:13 -0500

    added workerm handler

--- static/index.js
+++ static/index.js
@@ -1,41 +1,53 @@
-fetch("/api/worker")
-  .then(function (response){
-    return response.json()
-  })
-  .then(function(worker){
-    console.log('test data',worker[0].name)
-  })
-
-  fetch("/api/equipment_type")
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-    }).catch(err => console.error(err));
-
 var app = new Vue({
   el: "#container",
   data: {
     user: "home",
-    equipments:[
-        {key:1, name: "Pump"},
-        {key:2, name: "Compressor"},
-        {key:3, name: "Seperator"},
-        {key:4, name: "Sensor"},
-        {key:5, name: "Security"},
-        {key:6, name: "Electricity"},
-        {key:7, name: "Networking"},
-        {key:8, name: "Vehicle"},
-        {key:9, name: "HVAC"},
-        {key:10, name: "Conveyer"}
-    ],
-    aSelectedEquipment:1,
-    facilities:[
-        {key:1, name:"Facility 1"},
-        {key:2, name:"Facility 2"},
-        {key:3, name:"Facility 3"},
-        {key:4, name:"Facility 4"},
-        {key:5, name:"Facility 5"},
-    ],
-    aSelectedFacility:1
+    types: [],
+    facilities: [],
+    aWorkerName: "",
+    aWorkerShift: ""
+  },
+  mounted() {
+    this.adminFiller();
+  },
+  methods: {
+    adminFiller: () => {
+      fetch("/api/equipment_type")
+        .then(res => res.json())
+        .then(data => {
+          this.types = data;
+        })
+        .catch(err => console.error(err));
+
+      fetch("api/facility")
+        .then(res => res.json())
+        .then(data => {
+          this.facilities = data;
+        })
+        .catch(err => console.error(err));
+
+      fetch("api/certification")
+        .then(res => res.json())
+        .then(data => {
+          this.certificates = data;
+        })
+        .catch(err => console.error(err));
+    },
+
+    addNewWorker: () => {
+      fetch("/api/worker", {
+        method: "POST",
+        body: {
+          name: this.aWorkerName,
+          shift: this.aWorkerShift
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          this.aWorkerName = "";
+          this.aWorkerShift = "";
+        })
+        .catch(err => console.error(err));
+    }
   }
 });
