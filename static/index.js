9a70a0b73ba8a76399e2580bdcc3fc0ebd6e571a
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-09-15 16:03:08 -0500

    added callback functions

--- static/index.js
+++ static/index.js
@@ -4,14 +4,18 @@ var app = new Vue({
     user: "home",
     types: [],
     facilities: [],
+    aLongitude:"",
+    aLatitude:"",
     aWorkerName: "",
-    aWorkerShift: ""
+    aWorkerShift: "",
+    aNewEquipmentType:""
   },
   mounted() {
     this.adminFiller();
   },
   methods: {
-    adminFiller: () => {
+    //get equipmentType and list of facilities number
+    adminFiller: function() {
       fetch("/api/equipment_type")
         .then(res => res.json())
         .then(data => {
@@ -25,21 +29,18 @@ var app = new Vue({
           this.facilities = data;
         })
         .catch(err => console.error(err));
-
-      fetch("api/certification")
-        .then(res => res.json())
-        .then(data => {
-          this.certificates = data;
-        })
-        .catch(err => console.error(err));
     },
 
-    addNewWorker: () => {
+    //add new worker's name and shift
+    addNewWorker: function() {
       fetch("/api/worker", {
-        method: "POST",
-        body: {
-          name: this.aWorkerName,
-          shift: this.aWorkerShift
+        method: "post",
+        body: JSON.stringify({
+          'name': this.aWorkerName,
+          'shift': this.aWorkerShift
+        }),
+        headers: {
+          'Content-Type': 'application/json'
         }
       })
         .then(res => res.json())
@@ -48,6 +49,44 @@ var app = new Vue({
           this.aWorkerShift = "";
         })
         .catch(err => console.error(err));
+    },
+
+    //add facility latitude and longitude
+    addNewFacility: function(){
+      fetch("/api/facility",{
+        method:"post",
+        body: JSON.stringify({
+          'lat':this.aLongitude,
+          'lon':this.aLatitude
+        }),
+        headers:{
+          'Content-Type':'application/json'
+        }
+      })
+      .then(res=>res.json())
+      .then(data=>{
+        this.aLatitude="";
+        this.aLongitude="";
+      })
+    },
+
+    //add new equipment type
+    addNewEquipmentType: function(){
+      fetch("/api/equipment_type",{
+        method:"post",
+        body: JSON.stringify({
+          'name':this.aNewEquipmentType
+        }),
+        headers:{
+          'Content-Type':'application/json'
+        }
+      })
+      .then(res=>res.json())
+      .then(data=>{
+        this.addNewEquipmentType="";
+        this.adminFiller();
+      })
+
     }
-  }
+  } 
 });
