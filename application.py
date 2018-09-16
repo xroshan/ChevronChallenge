1419536094039b36de21112cdc3bbfac37a3e8b6
muedie <lasafalus@gmail.com>
2018-09-16 01:51:23 -0500

    finished sanitizations

--- application.py
+++ application.py
@@ -40,6 +40,7 @@ def certification_root():
         if not Worker.query.get(data['worker_id']):
             return jsonify({"success": False, "message": "Worker key not found."}), 404
 
+        # add to db
         certification = Certification(data['equipment_type_id'], data['worker_id'])
         db.session.add(certification)
         db.session.commit()
@@ -75,6 +76,7 @@ def equipment_root():
         if not Facility.query.get(data['facility_id']):
             return jsonify({"success": False, "message": "Facility key not found."}), 404
 
+        # add to db
         equipment = Equipment(data['equipment_type_id'], data['facility_id'])
         db.session.add(equipment)
         db.session.commit()
@@ -94,6 +96,29 @@ def equipment_type_root():
         # expected data [name, prob, hour_min, hour_max]
         data = request.get_json()
 
+        # sanitization
+        if not data:
+            return jsonify({"success": False, "message": "Missing body."}), 400
+
+        if not 'name' in data or not 'prob' in data or not 'hour_min' in data or not 'hour_max' in data:
+            return jsonify({"success": False, "message": "Missing body fields."}), 400
+
+        if not isinstance(data['name'], str) or not isinstance(data['prob'], (int, float)) or not isinstance(data['hour_min'], int) or not isinstance(data['hour_max'], int):
+            return jsonify({"success": False, "message": "Invalid body fields."}), 400
+
+        if len(data['name'].strip()) < 1 or len(data['name']) > 25:
+            return jsonify({"success": False, "message": "Name length out of range."}), 404
+
+        if data['prob'] < 0 or data['prob'] > 1:
+            return jsonify({"success": False, "message": "Probability out of range."}), 404
+
+        if data['hour_min'] < 1:
+            return jsonify({"success": False, "message": "Minimum hour out of range."}), 404
+
+        if data['hour_max'] < 1 or data['hour_max'] < data['hour_min']:
+            return jsonify({"success": False, "message": "Maximum hour out of range."}), 404
+
+        # add to db
         e_type = EquipmentType(data['name'], data['prob'], data['hour_min'], data['hour_max'])
         db.session.add(e_type)
         db.session.commit()
@@ -116,6 +141,23 @@ def facility_root():
         # expected data [lat, lon]
         data = request.get_json()
 
+        # sanitization
+        if not data:
+            return jsonify({"success": False, "message": "Missing body."}), 400
+
+        if not 'lat' in data or not 'lon' in data:
+            return jsonify({"success": False, "message": "Missing body fields."}), 400
+
+        if not isinstance(data['lat'], (int, float)) or not isinstance(data['lon'], (int, float)):
+            return jsonify({"success": False, "message": "Invalid body fields."}), 400
+
+        if data['lat'] < -90.0 or data['lat'] > 90.0:
+            return jsonify({"success": False, "message": "Latitude out of range."}), 404
+
+        if data['lon'] < -180.0 or data['lon'] > 180.0:
+            return jsonify({"success": False, "message": "Longitude out of range."}), 404
+
+        # add to db
         facility = Facility(data['lat'], data['lon'])
         db.session.add(facility)
         db.session.commit()
@@ -154,6 +196,29 @@ def order_root():
         # expected data [priority, time_to_completion, facility_id, equipment_id]
         data = request.get_json()
 
+        # sanitization
+        if not data:
+            return jsonify({"success": False, "message": "Missing body."}), 400
+
+        if not 'priority' in data or not 'time_to_completion' in data or not 'facility_id' in data or not 'equipment_id' in data:
+            return jsonify({"success": False, "message": "Missing body fields."}), 400
+
+        if not isinstance(data['priority'], int) or not isinstance(data['time_to_completion'], int) or not isinstance(data['facility_id'], int) or not isinstance(data['equipment_id'], int):
+            return jsonify({"success": False, "message": "Invalid body fields."}), 400
+
+        if data['priority'] < 1 or data['priority'] > 5:
+            return jsonify({"success": False, "message": "Priority out of range."}), 404
+
+        if data['time_to_completion'] < 1:
+            return jsonify({"success": False, "message": "Time to completion out of range."}), 404
+
+        if not Equipment.query.get(data['equipment_id']):
+            return jsonify({"success": False, "message": "Equipment key not found."}), 404
+
+        if not Facility.query.get(data['facility_id']):
+            return jsonify({"success": False, "message": "Facility key not found."}), 404
+
+        # add to db
         order = Order(data['priority'], data['time_to_completion'], data['facility_id'], data['equipment_id'])
         db.session.add(order)
         db.session.commit()
@@ -170,9 +235,26 @@ def order_root():
 def worker_root():
     
     if request.method == "POST":
-        # expected data [name, shift, certificaions[](optional)]
+        # expected data [name, shift]
         data = request.get_json()
 
+        # sanitization
+        if not data:
+            return jsonify({"success": False, "message": "Missing body."}), 400
+
+        if not 'name' in data or not 'shift' in data:
+            return jsonify({"success": False, "message": "Missing body fields."}), 400
+
+        if not isinstance(data['name'], str) or not isinstance(data['shift'], str):
+            return jsonify({"success": False, "message": "Invalid body fields."}), 400
+
+        if len(data['name'].strip()) < 1 or len(data['name']) > 100:
+            return jsonify({"success": False, "message": "Name length out of range."}), 404
+
+        if len(data['shift'].strip()) < 1 or len(data['shift']) > 15:
+            return jsonify({"success": False, "message": "Shift length out of range."}), 404
+
+        # add to db
         worker = Worker(data['name'], data['shift'])
         db.session.add(worker)
         db.session.commit()
